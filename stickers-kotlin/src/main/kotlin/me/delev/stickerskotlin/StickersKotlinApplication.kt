c28ea8261dfa3d31f1757be5d41f649215b7a211
Tomche Delev <tomce.delev@gmail.com>
2019-03-29 23:45:35 +0100

    Kotlin starter

---
+++ stickers-kotlin/src/main/kotlin/me/delev/stickerskotlin/StickersKotlinApplication.kt
@@ -0,0 +1,11 @@
+package me.delev.stickerskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StickersKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<StickersKotlinApplication>(*args)
+}
